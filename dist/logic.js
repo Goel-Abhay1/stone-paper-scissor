43d756f7911fe91e6709565a1b5ea3c0ec052291
Abhay Goel <138133907+Goel-Abhay1@users.noreply.github.com>
2023-12-15 02:55:17 +0530

    done many things
    learned to create seperate function while event handling
    clean code

--- dist/logic.js
+++ dist/logic.js
@@ -10,7 +10,10 @@ let countTie = 0;
 let arr = [rock,paper,scissor]
 const randomChoice = ()=>{
     let avlChoice = ["rock","paper","scissor"]
-    return avlChoice[Math.floor(Math.random() * 3)]
+    let temp_var = Math.floor(Math.random() * 3)
+    computerStylingTask(temp_var)
+    return avlChoice[temp_var]
+    
 }
 const checkFor =(userChoice,computerChoice )=>{
     if(userChoice == computerChoice){
@@ -69,18 +72,71 @@ const updateCount = ()=>{
     loseHTML.innerText = `Lose : ${countLose}`
     tieHTML.innerText = `Tie : ${countTie}`
 }
-for(let block of arr){
-    for(let content of block){
-        content.addEventListener("click",()=>{
-            let userChoice = content.getAttribute("value")
-            let computerChoice = randomChoice();
-            checkFor(userChoice,computerChoice);
-        })
-    }
+const handleContentClick=(event)=> {
+    let userChoice = event.currentTarget.getAttribute("value");
+    userStylingTask(userChoice);
+    let computerChoice = randomChoice();
+    checkFor(userChoice, computerChoice);
 }
+
+const initialEventAdder = ()=>{for(let block of arr){
+    for(let content of block){
+        content.addEventListener("click",handleContentClick)
+    }
+}}
+initialEventAdder()
 
 let newGame  = document.querySelector("#newGame")
 newGame.addEventListener("click",()=>{
     countLose=countTie=countWin=0;
-    
+    updateCount()
+    for(let choices of computerChoiceArr){
+        for(content of choices){
+            content.style.border = "5px solid white"
+        }
+    }
+    for(let choices of arr){
+        for(content of choices){
+            content.style.border = "5px solid white"
+        }
+    }
+    initialEventAdder()
+        
 })
+
+// computer side programming
+
+let computerRock = [
+    document.querySelector("#computerRockImg"),
+    document.querySelector("#computerCaptionRock")
+]
+
+let computerPaper = [
+    document.querySelector("#computerPaperImg"),
+    document.querySelector("#computerCaptionPaper")
+]
+
+let computerScissor = [
+    document.querySelector("#computerScissorImg"),
+    document.querySelector("#computerCaptionScissor")
+]
+
+let computerChoiceArr = [computerRock,computerPaper,computerScissor];
+
+const computerStylingTask = (idx)=>{
+    for(let i of computerChoiceArr[idx]){
+        i.style.border = "5px solid yellow"
+    }
+}
+
+const userStylingTask = (choice)=>{
+    let bothChoice = document.querySelectorAll(`[value="${choice}"]`)
+    for(let content of bothChoice){
+        content.style.border = "5px solid magenta"
+    }
+    for(let choice of arr){
+        for(let content of choice){
+            content.removeEventListener("click",handleContentClick)
+        }
+    }
+}
